c265cd87d75428460ed915176bc1368e2231a82e
andreasx7 <andreasx7dw@gmail.com>
2025-05-10 20:11:43 +0300

    basic homepage implementation

--- src/app/components/home/cmp-template.component.ts
+++ src/app/components/home/cmp-template.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 
 
@@ -11,6 +12,7 @@ export class HomeComponent{
 
   
   constructor(
+    private router: Router,
   ) {
   }
 
@@ -18,4 +20,18 @@ export class HomeComponent{
    
     }
   
+  navigateTo(project:any){
+    switch(project){
+      case 1: 
+        window.open('https://boss.webdevjourney.site', '_blank');
+        break;
+      case 2: 
+        window.open('https://pilotta.webdevjourney.site', '_blank');
+        break;
+      case 3: 
+        window.open('https://dasproject.webdevjourney.site', '_blank');
+        break;
+    }
+
+  }
 }
